a954e628135d2cb81acad71940f55700e43ff2a2
ditclear <weiyixing@hd123.com>
2018-06-01 14:18:16 +0800

    init

--- MVVM-IOS/view/viewmodel/ViewController.swift
+++ MVVM-IOS/view/viewmodel/ViewController.swift
@@ -7,19 +7,18 @@
 //
 
 import UIKit
+import RxSwift
+
 
 class ViewController: UIViewController {
 
+
+    let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ Podfile
@@ -0,0 +1,8 @@
+use_frameworks!
+
+target 'MVVM-IOS' do
+    pod 'RxSwift',    '~> 4.0'
+    pod 'RxCocoa',    '~> 4.0'
+    pod 'RxDataSources', '~> 3.0'
+    pod 'RxAlamofire', '~> 4.0'
+end

025e0d5327dfd96e47161df8110467cd6ec505d8
ditclear <weiyixing@hd123.com>
2018-06-01 11:28:44 +0800

    Initial Commit

---
+++ MVVM-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVM-IOS
+//
+//  Created by ditclear on 2018/6/1.
+//  Copyright © 2018年 ditclear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
